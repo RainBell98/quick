7635a7485410cfa121aaa30259097a829abff774
RainBell98 <ttiger45@naver.com>
2024-02-27 13:39:17 +0900

    fix updateUserDto

--- src/dto/update-user.dto.ts
+++ src/dto/update-user.dto.ts
@@ -1,7 +1,9 @@
 import {IsOptional, IsString} from "class-validator";
+import {CreateUserDto} from "./create-user.dto";
+import {PickType} from "@nestjs/mapped-types";
+import {User} from "../Entity/auth.entity";
 
-export class UpdateUserDto{
-    @IsString()
+export class UpdateUserDto extends PickType(User, ['username']){
     @IsOptional()
     username: string
 }
